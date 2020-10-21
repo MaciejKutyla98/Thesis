9fd30a3146c532e79669f23f47d90dc685c7a9a7
maciejkutyla <maciejo117@gmail.com>
2020-10-21 19:04:03 +0200

    Login and registration layouts

---
+++ app/src/main/java/com/example/thesis/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.thesis;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToRegisterActivity(View view){
+        Intent intent = new Intent(this, Registration.class);
+        startActivity(intent);
+        finish();
+    }
+}
