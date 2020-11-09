1498718880db4675590373e2b7e2140efad7f4da
maciejkutyla <maciejo117@gmail.com>
2020-11-09 16:11:45 +0100

    Added basic calendar View

--- app/src/main/java/com/example/thesis/Calendar.java
+++ app/src/main/java/com/example/thesis/Calendar.java
@@ -1,24 +1,19 @@
 package com.example.thesis;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class News extends AppCompatActivity {
+public class Calendar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news);
-
-
+        setContentView(R.layout.activity_calendar);
     }
 
     @Override
@@ -101,4 +96,6 @@ public class News extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+
 }
